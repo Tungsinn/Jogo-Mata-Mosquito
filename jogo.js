163b5a828e427224794710c613298d3513009ccc
Tungsinn <gvnalamas@tutanota.com>
2020-12-15 22:09:22 -0300

    Initial commit

---
+++ jogo.js
@@ -0,0 +1,11 @@
+altura = 0
+largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+}
+
+ajustaTamanhoPalcoJogo()
+
+console.log(altura, largura)
