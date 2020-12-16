05956dbc9ffec63a976a8015df8fad9cbe3f3192
Tungsinn <gvnalamas@tutanota.com>
2020-12-15 22:49:48 -0300

    Geração de posições radômicas na tela

--- jogo.js
+++ jogo.js
@@ -1,11 +1,28 @@
 altura = 0
 largura = 0
 
+// Ajusta as variáveis ao tamanho da tela
 function ajustaTamanhoPalcoJogo() {
     var altura = window.innerHeight
     var largura = window.innerWidth
 }
-
 ajustaTamanhoPalcoJogo()
 
-console.log(altura, largura)
+function posicaoRandomica() {
+    // Gera posições randômicas para o mosquito na tela
+    // Math.floor() para arrendondar as casas decimais
+    var posicaoX = Math.floor(Math.random() * largura)
+    var posicaoY = Math.floor(Math.random() * altura)
+
+    console.log(posicaoX, posicaoY)
+
+    // Criar o elemento HTML dinamicamente
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosca.png"
+    mosquito.className = "mosquito1"
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
