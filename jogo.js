4ffb1c9b32d67c567d468818a95029c9112b9382
Tungsinn <gvnalamas@tutanota.com>
2020-12-16 23:23:17 -0300

    Criação da função tamanhoAleatorio()

--- jogo.js
+++ jogo.js
@@ -26,10 +26,27 @@ function posicaoRandomica() {
     // Criar o elemento HTML dinamicamente
     var mosquito = document.createElement('img')
     mosquito.src = "imagens/mosca.png"
-    mosquito.className = "mosquito1"
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+    // Produz valores entre 0 e 2
+    var classe = Math.floor(Math.random() * 3)
+
+    // Retorna o nome da classe que será aplicada ao elemento 'mosquito'
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
 }
